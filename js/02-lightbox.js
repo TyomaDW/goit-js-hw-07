8dd3d7af6d4bf33044a7a98c6d48975f012071ca
Tyoma <risendrums777@gmail.com>
2021-10-20 23:01:36 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,16 +5,26 @@ const galleryRef = document.querySelector('.gallery');
 
 const imgMarkup = galleryItems
   .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
-	            <a class="gallery__link" href="${original}">
-		            <img
-			            class="gallery__image"
-			            src="${preview}"
-			            data-source="${original}"
-			            alt="${description}"/>
-	            </a>
-            </div>`;
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
   })
   .join('');
 
 galleryRef.insertAdjacentHTML('beforeend', imgMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+const onModalOpen = e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  } else {
+    lightbox.open();
+  }
+};
+galleryRef.addEventListener('click', onModalOpen);
